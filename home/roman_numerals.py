2e020ce71559a4549b97d1694d643e480a1fda51
Taro Yamashita <taro.ymst@gmail.com>
2016-10-13 08:12:44 +0900

    Solved roman-numerals

---
+++ home/roman_numerals.py
@@ -0,0 +1,22 @@
+def checkio(data):
+    # replace this for solution
+    romans_table = [['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+                    ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
+                    ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
+                    ['M', 'MM', 'MMM']]
+    romans = []
+
+    for i, s in enumerate(str(data)[::-1]):
+        if s == '0':
+            continue
+        idx = int(s) - 1
+        romans.append(romans_table[i][idx])
+
+    return ''.join(romans[::-1])
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(6) == 'VI', '6'
+    assert checkio(76) == 'LXXVI', '76'
+    assert checkio(499) == 'CDXCIX', '499'
+    assert checkio(3888) == 'MMMDCCCLXXXVIII', '3888'
