20a52a9e5190a535421fd7442f86cfc38bd1803a
Taro Yamashita <taro.ymst@gmail.com>
2016-10-19 07:44:04 +0900

    Solved binary-count.

---
+++ electronic_station/binary_count.py
@@ -0,0 +1,10 @@
+def checkio(number):
+    b = bin(number)
+    return b.count('1')
+
+
+if __name__ == '__main__':
+    assert checkio(4) == 1
+    assert checkio(15) == 4
+    assert checkio(1) == 1
+    assert checkio(1022) == 9
