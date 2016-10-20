0b02826c0f03fa4e25eefa34f8b04dcb669ca621
Taro Yamashita <taro.ymst@gmail.com>
2016-10-21 07:52:23 +0900

    Solved median.

---
+++ oreilly/median.py
@@ -0,0 +1,18 @@
+def checkio(data):
+    data.sort()
+    l = len(data)
+    idx = l // 2
+    if not l % 2 == 0:
+        return data[idx]
+    else:
+        return (data[idx - 1] + data[idx]) / 2
+
+
+if __name__ == '__main__':
+    assert checkio([1, 2, 3, 4, 5]) == 3, "Sorted list"
+    assert checkio([3, 1, 2, 5, 3]) == 3, "Not sorted list"
+    assert checkio([1, 300, 2, 200, 1]) == 2, "It's not an average"
+    assert checkio([3, 6, 20, 99, 10, 15]) == 12.5, "Even length"
+    print("Start the long test")
+    assert checkio(list(range(1000000))) == 499999.5, "Long."
+    print("The local tests are done.")
