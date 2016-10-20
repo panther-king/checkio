4eec179ea85fa2c170315005ee443e0f10fed4aa
Taro Yamashita <taro.ymst@gmail.com>
2016-10-21 07:52:49 +0900

    Solved i-love-python.

---
+++ oreilly/i_love_python.py
@@ -0,0 +1,16 @@
+def i_love_python():
+    """
+    - Very easy to learn
+    - Simple but powerful
+    - Buttery include
+    - Many useful libraries in pip
+    - Coding-style is unified by PEP
+    - Complehensions
+    - Decorator
+    - `with` statement
+    - All error returns an exception
+    """
+    return "I love Python" + "!"
+
+if __name__ == '__main__':
+    assert i_love_python() == "I love Python!"
